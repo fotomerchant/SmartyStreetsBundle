2a03375a2e2b1cd2a3023cfa4fc74f3ad41c0789
Derek Clapham <derek.clapham@fotomerchant.com>
2020-06-19 15:59:25 +1000

    Updated SmartyStreetsBundle for Symfony 4.4+

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('smarty_streets');
+        $treeBuilder = new TreeBuilder('smarty_streets');
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
